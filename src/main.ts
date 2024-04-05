86309738d0fe769b3e89a476bca34a64d7561103
jonesp1 <jonas.poulsen@lego.com>
2024-04-05 14:38:16 +0200

    reverseWords

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,16 @@
-function reverseVowels(s: string): string {
-    const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
-    const chars = s.split('');
-    let left = 0;
-    let right = s.length - 1;
-
-    while (left < right) {
-        if (vowels.has(chars[left]) && vowels.has(chars[right])) {
-            [chars[left], chars[right]] = [chars[right], chars[left]];
-            left++;
-            right--;
-        } else if (vowels.has(chars[left])) {
-            right--;
-        } else {
-            left++;
-        }
-    }
-
-    return chars.join('');
+function reverseWords(s: string): string {
+    return s.split(' ').reverse().join(' ');
 }
 
 //example
-console.log(reverseVowels('hello'));
+// Measure the runtime
+const startTime = Date.now();
+const result = reverseWords('the sky is blue');
+const endTime = Date.now();
+
+// Calculate the runtime in milliseconds
+const runtimeInMs = endTime - startTime;
+
+// Log the result and runtime
+console.log("Reversed words:", result);
+console.log("Runtime in milliseconds:", runtimeInMs);
