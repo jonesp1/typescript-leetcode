12ff87a873e9f8687e06a4556ac528a75c1cc080
jonesp1 <jonas.poulsen@lego.com>
2024-04-08 13:52:33 +0200

    Two pointers: maxArea

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,17 @@
-function isSubsequence(s: string, t: string): boolean {
+function maxArea(height: number[]): number {
+    let max = 0;
     let i = 0;
-    let j = 0;
-    
-    while (i < s.length && j < t.length) {
-        if (s[i] === t[j]) {
+    let j = height.length - 1;
+    while (i < j) {
+        max = Math.max(max, Math.min(height[i], height[j]) * (j - i));
+        if (height[i] < height[j]) {
         i++;
+        } else {
+        j--;
         }
-        j++;
     }
-    
-    return i === s.length;
+    return max;
 }
 
 //example
-let s = "abc";
-let t = "ahbgdc";
-console.log(isSubsequence(s, t));
+console.log(maxArea([1,8,6,2,5,4,8,3,7]));
