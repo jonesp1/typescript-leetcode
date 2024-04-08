4f92fa12d3468c5c7eb4342c8aa79056da17718c
jonesp1 <jonas.poulsen@lego.com>
2024-04-08 13:53:50 +0200

    Two pointers: maxOperations

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
-function maxArea(height: number[]): number {
-    let max = 0;
-    let i = 0;
-    let j = height.length - 1;
-    while (i < j) {
-        max = Math.max(max, Math.min(height[i], height[j]) * (j - i));
-        if (height[i] < height[j]) {
-        i++;
-        } else {
-        j--;
+function maxOperations(nums: number[], k:number): number {
+    let count = 0;
+    let map = new Map();
+    for(let i = 0; i < nums.length; i++){
+        let diff = k - nums[i];
+        if(map.has(diff) && map.get(diff) > 0){
+            count++;
+            map.set(diff, map.get(diff) - 1);
+        }else{
+            map.set(nums[i], (map.get(nums[i]) || 0) + 1);
         }
     }
-    return max;
+    return count;
 }
 
 //example
-console.log(maxArea([1,8,6,2,5,4,8,3,7]));
+console.log(maxOperations([1,2,3,4], 5));
