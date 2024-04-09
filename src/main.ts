62cc6aaab9d4f09c3909e99090d32f71983dd70b
jonesp1 <jonas.poulsen@lego.com>
2024-04-09 13:31:02 +0200

    Prefix sum: largestAltitude

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,14 @@
-function longestSubarray(nums: number[]): number {
-    let left = 0;
-    let right = 0;
-    let zeroCount = 0;
+function largestAltitude(gain: number[]): number {
     let max = 0;
-    while(right < nums.length) {
-        if(nums[right] === 0) {
-            zeroCount++;
+    let current = 0;
+    for (let i = 0; i < gain.length; i++) {
+        current += gain[i];
+        if (current > max) {
+            max = current;
         }
-        while(zeroCount > 1) {
-            if(nums[left] === 0) {
-                zeroCount--;
-            }
-            left++;
-        }
-        max = Math.max(max, right - left);
-        right++;
     }
     return max;
 }
 
 //example
-console.log(longestSubarray([1,1,0,0,1,1,1,0,1]));
+console.log(largestAltitude([1,1,0,0,1,1,1,0,1]));
