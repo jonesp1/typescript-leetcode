e163cc65d3ef0e7d68b6a99491fedbd7bc668771
jonesp1 <jonas.poulsen@lego.com>
2024-04-09 20:27:43 +0200

    Stack: removeStars

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,17 @@
-function equalPairs(grid: number[][]): number {
-    const n = grid.length;
-    let count = 0;
+function removeStars(s: string): string {
+    const stack: string[] = [];
 
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            if (isEqualRowColumn(grid, i, j)) {
-                count++;
-            }
+    for (const char of s) {
+        if (char === '*') {
+            stack.pop(); // Remove the closest non-star character to the left
+        } else {
+            stack.push(char);
         }
     }
 
-    return count;
-}
-
-function isEqualRowColumn(grid: number[][], rowIndex: number, colIndex: number): boolean {
-    const n = grid.length;
-
-    // Check if row and column have same length
-    if (grid[rowIndex].length !== n || colIndex >= n) {
-        return false;
-    }
-
-    // Check if elements of row and column are equal
-    for (let i = 0; i < n; i++) {
-        if (grid[rowIndex][i] !== grid[i][colIndex]) {
-            return false;
-        }
-    }
-
-    return true;
+    return stack.join('');
 }
 
 //example
-console.log(equalPairs([[3, 2, 1], [1, 7, 6], [2, 7, 7]]));
+console.log(removeStars('ab*cd*ef'))
 
