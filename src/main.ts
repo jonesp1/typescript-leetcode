e812166f5988e5c2f2c2ef9205b912a887ed2272
jonesp1 <jonas.poulsen@lego.com>
2024-04-09 20:21:37 +0200

    Hashmap / sets: closeStrings

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,33 @@
-function uniqueOccurrences(arr: number[]): boolean {
-    const countMap: Map<number, number> = new Map();
+function closeStrings(word1: string, word2: string): boolean {
+    if(word1.length !== word2.length) return false;
 
-    for (const num of arr) {
-        countMap.set(num, (countMap.get(num) || 0) + 1);
+    const freqMap1: Map<string, number> = new Map();
+    const freqMap2: Map<string, number> = new Map();
+
+    for(const char of word1) {
+        freqMap1.set(char, (freqMap1.get(char) || 0) + 1);
     }
 
-    const occurencesSet = new Set(countMap.values());
-    return occurencesSet.size === countMap.size;
-};
+    for(const char of word2) {
+        freqMap2.set(char, (freqMap2.get(char) || 0) + 1);
+    }
+
+    const set1 = new Set(freqMap1.values());
+    const set2 = new Set(freqMap2.values());
+
+    const chars1 = new Set(freqMap1.keys());
+    const chars2 = new Set(freqMap2.keys());
+
+    return setEqual(set1, set2) && setEqual(chars1, chars2);
+}
+    function setEqual(set1: Set<any>, set2: Set<any>): boolean {
+        if(set1.size !== set2.size) return false;
+        for(const val of set1) {
+            if(!set2.has(val)) return false;
+        }
+        return true;
+}
 
 //example
-console.log(uniqueOccurrences([1, 2, 2, 1, 1, 3]));
+console.log(closeStrings("abc", "bca"));
 
